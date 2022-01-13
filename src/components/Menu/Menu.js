96d07b1452343020c89cc597e4cdebc0f9a9d95a
Kirill Makhov <mmahovkirill@gmail.com>
2022-01-13 23:36:56 +0300

    fix: remove flicker

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,32 +1,46 @@
 import cn from 'classnames';
 import s from './Menu.module.css';
 
+const MENU = [
+    {
+        title: 'HOME',
+        to: '#welcome',
+    },
+    {
+        title: 'GAME',
+        to: '#game',
+    },
+    {
+        title: 'ABOUT',
+        to: '#about',
+    },
+    {
+        title: 'CONTACT',
+        to: '#contact',
+    },
+];
+
 function Menu({active}) {
     return (
-        <div className={cn(s.menuContainer, {[s.active]: active})}>
+        <div className={
+            cn(s.menuContainer,
+                {
+                    [s.active]: active === true,
+                    [s.deactive]: active === false
+                })
+        }>
             <div className={s.overlay}/>
             <div className={s.menuItems}>
                 <ul>
-                    <li>
-                        <a href="#welcome">
-                            HOME
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#game">
-                            GAME
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#about">
-                            ABOUT
-                        </a>
-                    </li>
-                    <li>
-                        <a href="#contact">
-                            CONTACT
-                        </a>
-                    </li>
+                    {
+                        MENU.map(item => (
+                            <li>
+                                <a href={item.to}>
+                                    {item.title}
+                                </a>
+                            </li>
+                        ))
+                    }
                 </ul>
             </div>
         </div>
