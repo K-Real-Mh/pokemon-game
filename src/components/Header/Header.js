a1b2b540fc77b910776fe9dca018266f63a065e8
Kirill Makhov <mmahovkirill@gmail.com>
2022-01-16 21:29:42 +0300

    feat: add silhouette and moon pictures to  Header component background

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,6 +12,8 @@ function Header({title, descr}) {
     return (
         <header className={s.root}>
             <div className={s.forest}/>
+            <div className={s.silhouette}/>
+            <div className={s.moon}/>
             <div className={s.container}>
                 {title && <h1>{title}</h1>}
                 {descr && <p>{descr}</p>}
