d8c4ecc879a54111ba633338c1df9ba01a59cca1
Kirill Makhov <mmahovkirill@gmail.com>
2022-01-30 23:48:33 +0300

    feat: add pokemons from database

--- src/routes/GamePage/GamePage.js
+++ src/routes/GamePage/GamePage.js
@@ -3,146 +3,18 @@ import {useHistory} from "react-router-dom";
 import Layout from "../../components/Layout/Layout";
 import s from "./GamePage.module.css";
 import PokemonCard from "../../components/PokemonCard/PokemonCard";
-import {useState} from "react";
-
-const POKEMONS = [
-    {
-        "abilities": [
-            "keen-eye",
-            "tangled-feet",
-            "big-pecks"
-        ],
-        "stats": {
-            "hp": 63,
-            "attack": 60,
-            "defense": 55,
-            "special-attack": 50,
-            "special-defense": 50,
-            "speed": 71
-        },
-        "type": "flying",
-        "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png",
-        "name": "pidgeotto",
-        "base_experience": 122,
-        "height": 11,
-        "id": 17,
-        "values": {
-            "top": "A",
-            "right": 2,
-            "bottom": 7,
-            "left": 5
-        }
-    },
-    {
-        "abilities": [
-            "intimidate",
-            "shed-skin",
-            "unnerve"
-        ],
-        "stats": {
-            "hp": 60,
-            "attack": 95,
-            "defense": 69,
-            "special-attack": 65,
-            "special-defense": 79,
-            "speed": 80
-        },
-        "type": "poison",
-        "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/24.png",
-        "name": "arbok",
-        "base_experience": 157,
-        "height": 35,
-        "id": 24,
-        "values": {
-            "top": 5,
-            "right": 9,
-            "bottom": "A",
-            "left": "A"
-        }
-    },
-    {
-        "abilities": [
-            "static",
-            "lightning-rod"
-        ],
-        "stats": {
-            "hp": 35,
-            "attack": 55,
-            "defense": 40,
-            "special-attack": 50,
-            "special-defense": 50,
-            "speed": 90
-        },
-        "type": "electric",
-        "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
-        "name": "pikachu",
-        "base_experience": 112,
-        "height": 4,
-        "id": 25,
-        "values": {
-            "top": 8,
-            "right": "A",
-            "bottom": 9,
-            "left": 6
-        }
-    },
-    {
-        "abilities": [
-            "overgrow",
-            "chlorophyll"
-        ],
-        "stats": {
-            "hp": 45,
-            "attack": 49,
-            "defense": 49,
-            "special-attack": 65,
-            "special-defense": 65,
-            "speed": 45
-        },
-        "type": "grass",
-        "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-        "name": "bulbasaur",
-        "base_experience": 64,
-        "height": 7,
-        "id": 1,
-        "values": {
-            "top": 8,
-            "right": 4,
-            "bottom": 2,
-            "left": 7
-        }
-    },
-    {
-        "abilities": [
-            "blaze",
-            "solar-power"
-        ],
-        "stats": {
-            "hp": 39,
-            "attack": 52,
-            "defense": 43,
-            "special-attack": 60,
-            "special-defense": 50,
-            "speed": 65
-        },
-        "type": "fire",
-        "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
-        "name": "charmander",
-        "base_experience": 62,
-        "height": 6,
-        "id": 4,
-        "values": {
-            "top": 7,
-            "right": 6,
-            "bottom": 1,
-            "left": 4
-        }
-    }
-]
+import {useEffect, useState} from "react";
+import database from "../../service/firebase";
 
 function GamePage() {
     const history = useHistory();
-    const [pokemons, setPokemons] = useState(POKEMONS);
+    const [pokemons, setPokemons] = useState({});
+
+    useEffect(() => {
+        database.ref('pokemons').once('value', (snapshot) => {
+            setPokemons(snapshot.val());
+        })
+    }, [])
 
     const handleClick = () => {
         history.push('/');
@@ -150,13 +22,17 @@ function GamePage() {
 
     const handleTurnPokemon = (id) => {
         setPokemons(prevState => {
-            const arrId = prevState.findIndex(pokemon => pokemon.id === id);
-            prevState[arrId] = {
-                ...prevState[arrId],
-                active: true
-            }
-            return [...prevState];
-        })
+            return Object.entries(prevState).reduce((acc, item) => {
+                const pokemon = {...item[1]};
+                if (pokemon.id === id) {
+                    pokemon.active = true;
+                }
+
+                acc[item[0]] = pokemon;
+
+                return acc;
+            }, {});
+        });
     }
 
     return (
@@ -165,8 +41,8 @@ function GamePage() {
             <Button onClick={handleClick}>Go to Home Page!</Button>
             <Layout id="cards" title="Cards" colorBg="#202736" colorTitle="#FEFEFE">
                 <div className={s.flex}>
-                    {pokemons.map(pokemon => (
-                        <PokemonCard key={pokemon.id} name={pokemon.name} id={pokemon.id} type={pokemon.type}
+                    {Object.entries(pokemons).map(([key, pokemon]) => (
+                        <PokemonCard key={key} name={pokemon.name} id={pokemon.id} type={pokemon.type}
                                      img={pokemon.img} values={pokemon.values} isActive={pokemon.active}
                                      setActive={handleTurnPokemon}/>
                     ))}
