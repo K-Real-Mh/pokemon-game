2ea9dcab1e3bbf617982d8f7c38d231e579e4ad1
Kirill Makhov <mmahovkirill@gmail.com>
2022-01-31 22:13:34 +0300

    feat: add addition new pokemon

--- src/routes/GamePage/GamePage.js
+++ src/routes/GamePage/GamePage.js
@@ -19,6 +19,49 @@ function GamePage() {
     const handleClick = () => {
         history.push('/');
     }
+    
+    const handleAddPokemon = () => {
+        const newKey = database.ref().child('pokemons').push().key;
+        const newPokemon = {
+            "abilities": [
+                "keen-eye",
+                "tangled-feet",
+                "big-pecks"
+            ],
+            "base_experience": 122,
+            "height": 11,
+            "weight": 300,
+            "id": new Date().getTime(),
+            "img": "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/17.png",
+            "name": "New Pokemon",
+            "stats": {
+                "hp": 63,
+                "attack": 60,
+                "defense": 55,
+                "special-attack": 50,
+                "special-defense": 50,
+                "speed": 71
+            },
+            "type": "normal",
+            "values": {
+                "top": 7,
+                "right": 5,
+                "bottom": 1,
+                "left": 2
+            }
+        };
+
+        setPokemons(prevState => {
+            return {
+                ...prevState,
+                [newKey]: newPokemon,
+            }
+        });
+
+        database.ref('pokemons/'+ newKey).set({
+            ...newPokemon
+        });
+    }
 
     const handleTurnPokemon = (id) => {
         setPokemons(prevState => {
@@ -42,6 +85,7 @@ function GamePage() {
         <>
             <h1>Game Page</h1>
             <Button onClick={handleClick}>Go to Home Page!</Button>
+            <Button onClick={handleAddPokemon}>Add new pokemon</Button>
             <Layout id="cards" title="Cards" colorBg="#202736" colorTitle="#FEFEFE">
                 <div className={s.flex}>
                     {Object.entries(pokemons).map(([key, pokemon]) => (
